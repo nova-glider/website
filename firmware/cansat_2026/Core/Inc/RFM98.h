dbbe01dbc137bb5c919799a98c2dfedccef0f13c
Sam Vandenabeele <sam.vandenabeele.09@gmail.com>
2025-06-05 16:14:33 +0200

    add headers like a less real sigma

--- firmware/cansat_2026/Core/Inc/RFM98.h
+++ firmware/cansat_2026/Core/Inc/RFM98.h
@@ -1,3 +1,29 @@
+/* USER CODE BEGIN header */
+/**
+  ******************************************************************************
+  * @file           : RFM98.h
+  * @brief          : RFM98.c include file
+  * @author         : Sam Vandenabeele
+  ******************************************************************************
+  * @attention
+  *
+  * This file is part of the NovaCan project.
+  *
+  * Copyright (C) 2025 NovaCan, Wannes Ghysels
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  ******************************************************************************
+**/
+/* USER CODE END header */
 #ifndef __LORA_RFM98_H
 #define __LORA_RFM98_H
 
