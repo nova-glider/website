b81392c589e919d315bc272d81e60808c8534733
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-08-20 19:50:01 +0200

    add theme toggler, toast and redirect wow

---
+++ app/live/page.tsx
@@ -0,0 +1,16 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
+
+export default function Live() {
+  return (
+    <div>
+      <div className="flex items-center justify-center min-h-screen">
+        
+      </div>
+      <div className="fixed top-4 right-4 z-50">
+        <ModeToggle />
+      </div>
+      
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+import { toast } from "sonner";
+
+import { useRouter } from 'next/navigation'
+
 import {
   AlertDialog,
   AlertDialogAction,
@@ -10,30 +16,56 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Toaster } from "@/components/ui/sonner"; // make sure to import from components/ui and not sonner directly
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const promise = () =>
+    new Promise((resolve) => setTimeout(() => resolve({ name: 'timeout' }), 500))
+      .then(() => {
+        router.push("/live");
+      });
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <AlertDialog defaultOpen={true}>
-        {/* <AlertDialogTrigger asChild>
+    <div>
+      <div className="flex items-center justify-center min-h-screen">
+        <AlertDialog defaultOpen={true}>
+          {/* <AlertDialogTrigger asChild>
           <Button variant="outline">Show Dialog</Button>
         </AlertDialogTrigger> */}
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Warning</AlertDialogTitle>
-            <AlertDialogDescription>
-              The NovaCan dashboard is under heavy development and might contain
-              hella bugs.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            {/* <AlertDialogCancel>Cancel</AlertDialogCancel> */}
-            <AlertDialogAction>I Understand</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Warning</AlertDialogTitle>
+              <AlertDialogDescription>
+                The NovaCan dashboard is under heavy development and might
+                contain hella bugs.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              {/* <AlertDialogCancel>Cancel</AlertDialogCancel> */}
+              <AlertDialogAction>I Understand</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
 
-      
+        <Button
+          variant="outline"
+          onClick={() => toast.promise(promise, {
+            loading: "Loading...",
+            success: "Redirecting to dashboard!",
+            error: "Failed to load dashboard."
+          })}
+        >
+          Enter Dashboard
+        </Button>
+      </div>
+      <div className="fixed top-4 right-4 z-50">
+        <ModeToggle />
+      </div>
+      <Toaster closeButton position="bottom-center" />
     </div>
   );
 }
