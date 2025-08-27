9656f3b0e42c96bd77c8b52d7fffd98e11cd4210
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-08-27 11:30:25 +0200

    remove unnecessary imports

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,11 @@ import { useRouter } from "next/navigation";
 import {
   AlertDialog,
   AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
