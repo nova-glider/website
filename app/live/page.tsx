957b6c22038277ecb7be6fd4d6459b2a03762a88
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-08-27 11:28:49 +0200

    add under construction type shit

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -1,16 +1,23 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
-
+import { Blockquote, Header2 } from "@/components/ui/typography";
 
 export default function Live() {
   return (
     <div>
-      <div className="flex items-center justify-center min-h-screen">
-        
+      <div className="flex flex-col items-center justify-center min-h-screen max-w-md mx-auto">
+        <div className="mb-4">
+          <Header2>Under Construction</Header2>
+        </div>
+        <Blockquote>
+          {/* "Patience, you must have, my young Padawan." <br /> - Yoda 
+            Apparently this quote isnt actually in the movies lol */}
+            
+          "This is only the beginning." <br /> ~ Winston Churchill
+        </Blockquote>
       </div>
       <div className="fixed top-4 right-4 z-50">
         <ModeToggle />
       </div>
-      
     </div>
   );
 }
