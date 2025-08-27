ca3a3ed08643939061aab7c860d03c2c525d7790
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-08-27 12:29:25 +0200

    change export config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/dashboard',
 };
 
 export default nextConfig;
