3b60712c905494b406fc2ea7fef0f37d51d02920
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-12-19 15:26:34 +0100

    remove async

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -53,14 +53,14 @@ export default function Live() {
 
   const latestDataHandler = () => {
     getLatestData()
-    .then(setLatestData)
-    .catch((error) => {
-      console.error("Failed to fetch latest data:", error);
-    })
-    .finally(() => {
-      setLoading(false);
-      setButtonLoading(false);
-    });
+      .then(setLatestData)
+      .catch((error) => {
+        console.error("Failed to fetch latest data:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+        setButtonLoading(false);
+      });
   };
 
   useEffect(() => {
@@ -90,7 +90,7 @@ export default function Live() {
 
       <div className="fixed bottom-25 left-1/2 -translate-x-1/2 z-50 flex justify-center">
         <Button
-          onClick={async () => {
+          onClick={() => {
             setButtonLoading(true);
             latestDataHandler();
           }}
