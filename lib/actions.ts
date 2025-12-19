c9eee6da1eca383c1022c4afed545e39e971a69f
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2025-12-19 15:25:42 +0100

    use map instead of for loop

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,24 +1,21 @@
-'use server';
+"use server";
 
 export async function getLatestData() {
-
   const res = await fetch(`${process.env.API_URL}/sensor-data/get/all`, {
-    cache: "no-store"
+    cache: "no-store",
   });
   const data = await res.json();
   const first4DataPoints = Array.isArray(data) ? data.slice(0, 4) : [];
-  const dataToShow = [];
-
-  for (let i = 0; i < first4DataPoints.length; i++) {
-    // add each data points timestamp, location.altitude, readings.temperature_celsius to dataToShow
-    dataToShow.push({
-      timestamp: first4DataPoints[i].timestamp,
-      altitude: first4DataPoints[i].location.altitude,
-      temperature: first4DataPoints[i].readings.temperature_celsius
-    });
-  }
   
-  dataToShow.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime());
+  const dataToShow = first4DataPoints.map((point) => ({
+    timestamp: point.timestamp,
+    altitude: point.location.altitude,
+    temperature: point.readings.temperature_celsius,
+  }));
+
+  dataToShow.sort(
+    (a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
+  );
 
   return dataToShow;
 }
