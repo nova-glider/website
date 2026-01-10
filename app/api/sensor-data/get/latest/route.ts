7908cec70ee5e45f547a0225fe4a982aff2545f0
wxnnvs <136503688+wxnnvs@users.noreply.github.com>
2026-01-10 11:12:44 +0100

    add copyright notices

--- app/api/sensor-data/get/latest/route.ts
+++ app/api/sensor-data/get/latest/route.ts
@@ -1,3 +1,19 @@
+/*
+ * This file is part of the NovaGlider project.
+ *
+ * Copyright (C) 2025 NovaGlider, Wannes Ghysels
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 import { NextRequest, NextResponse } from "next/server";
 import { initializeDb, getLatest, getCorsHeaders } from "@/lib/sensor-data";
 
